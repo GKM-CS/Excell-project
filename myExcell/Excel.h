e903da1daef3bdd0b55e73b6fa628ef2b9f0a770
GKM-CS <hyosin3536@gmail.com>
2026-03-18 20:56:38 +0900

    엑셀 입력 받아서 하기

---
+++ myExcell/Excel.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "utils.h"
+
+
+namespace MyExcel {
+	class Table;
+
+	class Excel {
+		Table* current_table;
+
+	public:
+		Excel(int max_row, int max_col, int choice);
+
+		int parse_user_input(string s);
+		void command_line();
+	};
+}
--- myExcell/main.cpp
+++ myExcell/main.cpp
@@ -2,43 +2,18 @@
 #include <fstream>
 #include "table.h"
 #include "cell.h"
+#include "Excel.h"
 
 int main() {
-    /*
-    MyExcel::TxtTable table(5, 5);
-    std::ofstream out("test.txt");
+    std::cout
+        << "테이블 (타입) (최대 행 크기) (최대 열 크기) 를 순서대로 입력해주세요" << std::endl;
+    std::cout << "* 참고 * " << std::endl;
+    std::cout << "1 : 텍스트 테이블, 2 : CSV 테이블, 3 : HTML 테이블" << std::endl;
 
-    table.reg_cell(new MyExcel::StringCell("Hello~", 0, 0, &table), 0, 0);
-    table.reg_cell(new MyExcel::StringCell("C++", 0, 1, &table), 0, 1);
-    table.reg_cell(new MyExcel::StringCell("Programming", 1, 1, &table), 1, 1);
-    std::cout << std::endl << table;
-    out << table;
+    int type, max_row, max_col;
+    std::cin >> type >> max_row >> max_col;
+    MyExcel::Excel m(max_row, max_col, type - 1);
+    m.command_line();
 
-    MyExcel::CSVTable csvTable(5, 5);
-    std::ofstream out2("test.csv");
-
-    csvTable.reg_cell(new MyExcel::StringCell("Hello~", 0, 0, &csvTable), 0, 0);
-    csvTable.reg_cell(new MyExcel::StringCell("C++", 0, 1, &csvTable), 0, 1);
-    csvTable.reg_cell(new MyExcel::StringCell("Programming", 1, 1, &csvTable), 1, 1);
-    out2 << csvTable;
-
-    MyExcel::HtmlTable htmlTable(5, 5);
-    std::ofstream out3("test.html");
-
-    htmlTable.reg_cell(new MyExcel::StringCell("Hello~", 0, 0, &htmlTable), 0, 0);
-    htmlTable.reg_cell(new MyExcel::StringCell("C++", 0, 1, &htmlTable), 0, 1);
-    htmlTable.reg_cell(new MyExcel::StringCell("Programming", 1, 1, &htmlTable), 1, 1);
-    out3 << htmlTable;
-    */
-    MyExcel::TxtTable table(5, 5);
-    table.reg_cell(new MyExcel::NumberCell(2, 1, 1, &table), 1, 1);
-    table.reg_cell(new MyExcel::NumberCell(3, 1, 2, &table), 1, 2);
-
-    table.reg_cell(new MyExcel::NumberCell(4, 2, 1, &table), 2, 1);
-    table.reg_cell(new MyExcel::NumberCell(5, 2, 2, &table), 2, 2);
-    table.reg_cell(new MyExcel::ExprCell("B2+B3*(C2+C3-2)", 3, 3, &table), 3, 2);
-    table.reg_cell(new MyExcel::StringCell("B2 + B3 * ( C2 + C3 - 2 ) = ", 3, 2, &table),
-        3, 1);
-    std::cout << table;
 
 }
